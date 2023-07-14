48983103f45ad0554bafec067497e1ed4adb72d5
Ermachkova Julia <miscanth@gmail.com>
2023-07-14 10:58:29 +0300

    add admin.py

---
+++ backend/cats/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from .models import Achievement, AchievementCat, Cat
+
+
+class CatAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'color',
+        'birth_year',
+        'owner',
+        'achievements'
+    )
+    empty_value_display = '-пусто-'
+
+
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class AchievementCatAdmin(admin.ModelAdmin):
+    list_display = (
+        'cat',
+        'achievements'
+    )
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Achievement, AchievementAdmin)
+admin.site.register(AchievementCat, AchievementCatAdmin)
+admin.site.register(Cat, CatAdmin)
